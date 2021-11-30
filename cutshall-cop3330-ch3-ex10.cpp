d524da2e398e0360d1b8a0ae53583f75be38e4d1
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-11-29 19:07:57 -0500

    Add files via upload

---
+++ cutshall-cop3330-ch3-ex10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Chase Cutshall
+ */
+using namespace std;
+
+int main()
+{
+string operation;
+double num1, num2;
+cout<<"Enter the expression: ";
+cin >> operation>>num1>>num2;
+if(operation == "+") {
+cout<<"Addition of two nums "<<num1<<" and "<<num2<<" is "<<(num1+num2)<<endl;
+}
+else if(operation == "-") {
+cout<<"Minus of two nums "<<num1<<" and "<<num2<<" is "<<(num1-num2)<<endl;
+}
+else if(operation == "*") {
+cout<<"Multiplication of two nums "<<num1<<" and "<<num2<<" is "<<(num1*num2)<<endl;
+} else if(operation == "/") {
+cout<<"Divition of two nums "<<num1<<" and "<<num2<<" is "<<(num1/num2)<<endl;
+}
+
+return 0;
+}
